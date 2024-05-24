0ce7ff83dd9d8a32bc5ca9d9fbd3ee22bee17ad6
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-05-24 14:56:37 -0700

    up to ggrs

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
+# progress
+up to "The rough outline of what we want to do now, is:"
+at https://johanhelsing.studio/posts/extreme-bevy
+
 # extreme_bevy
-johanhelsing example of matchbox and 
+johanhelsing example of matchbox and bevy from https://johanhelsing.studio/posts/extreme-bevy
+
+lots of install steps there, then run with
+
+    cargo run --target wasm32-unknown-unknown
+
+or better
+
+    cargo watch -cx "run --target wasm32-unknown-unknown"
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
+use bevy::{prelude::*,render::camera::ScalingMode};
+
+#[derive(Component)]
+struct Player;
+
 fn main() {
-    println!("Hello, world!");
+    App::new().add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            // fill the entire browser window
+            // TODO: re-enable in Bevy 0.14
+            // fit_canvas_to_parent: true,
+            // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
+            prevent_default_event_handling: false,
+            ..default()
+        }),
+        ..default()
+    }))
+    .insert_resource(ClearColor(Color::rgb(0.53, 0.53, 0.53))) 
+    .add_systems(Startup, (setup, spawn_player))
+    .add_systems(Update, move_player)
+    .run();
+}
+
+fn setup(mut commands: Commands) {
+    let mut camera_bundle = Camera2dBundle::default();
+    camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(10.);
+    commands.spawn(camera_bundle);
+}
+
+fn spawn_player(mut commands: Commands) {
+    commands.spawn((
+        Player,
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0., 0.47, 1.),
+                custom_size: Some(Vec2::new(1., 1.)),
+                ..default()
+            },
+            ..default()
+        },
+    ));
+}
+
+fn move_player(
+    mut players: Query<&mut Transform, With<Player>>,
+    keys: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    let mut direction = Vec2::ZERO;
+    if keys.any_pressed([KeyCode::ArrowUp, KeyCode::KeyW]) {
+        direction.y += 1.;
+    }
+    if keys.any_pressed([KeyCode::ArrowDown, KeyCode::KeyS]) {
+        direction.y -= 1.;
+    }
+    if keys.any_pressed([KeyCode::ArrowRight, KeyCode::KeyD]) {
+        direction.x += 1.;
+    }
+    if keys.any_pressed([KeyCode::ArrowLeft, KeyCode::KeyA]) {
+        direction.x -= 1.;
+    }
+    if direction == Vec2::ZERO {
+        return;
+    }
+
+    let move_speed = 7.;
+    let move_delta = direction * move_speed * time.delta_seconds();
+
+    for mut transform in &mut players {
+        transform.translation += move_delta.extend(0.);
+    }
 }
