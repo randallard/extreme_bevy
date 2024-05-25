cdb788df4e0bc833fcf64a51992377f84d00cf40
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-05-24 19:11:39 -0700

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-use bevy::{prelude::*,render::camera::ScalingMode};
+use bevy::{prelude::*, render::camera::ScalingMode, tasks::IoTaskPool};
+use bevy_ggrs::*;
+use matchbox_socket::{WebRtcSocket, PeerId};
+
+// The first generic parameter, u8, is the input type: 4-directions + fire fits
+// easily in a single byte
+// The second parameter is the address type of peers: Matchbox' WebRtcSocket
+// addresses are called `PeerId`s
+type Config = bevy_ggrs::GgrsConfig<u8, PeerId>;
 
 #[derive(Component)]
 struct Player;
@@ -16,8 +24,8 @@ fn main() {
         ..default()
     }))
     .insert_resource(ClearColor(Color::rgb(0.53, 0.53, 0.53))) 
-    .add_systems(Startup, (setup, spawn_player))
-    .add_systems(Update, move_player)
+    .add_systems(Startup, (setup, spawn_player, start_matchbox_socket))
+    .add_systems(Update, (move_player, wait_for_players))
     .run();
 }
 
@@ -69,4 +77,48 @@ fn move_player(
     for mut transform in &mut players {
         transform.translation += move_delta.extend(0.);
     }
+}
+
+fn start_matchbox_socket(mut commands: Commands) {
+    let room_url = "ws://127.0.0.1:3536/extreme_bevy?next=2";
+    info!("connecting to matchbox server: {room_url}");
+    commands.insert_resource(MatchboxSocket::new_ggrs(room_url));
+}
+
+fn wait_for_players(mut commands: Commands, mut socket: ResMut<MatchboxSocket<SingleChannel>>) {
+    if socket.get_channel(0).is_err() {
+        return; // we've already started
+    }
+
+    // Check for new connections
+    socket.update_peers();
+    let players = socket.players();
+
+    let num_players = 2;
+    if players.len() < num_players {
+        return; // wait for more players
+    }
+
+    info!("All peers have joined, going in-game");
+    
+    // create a GGRS P2P session
+    let mut session_builder = ggrs::SessionBuilder::<Config>::new()
+        .with_num_players(num_players)
+        .with_input_delay(2);
+
+    for (i, player) in players.into_iter().enumerate() {
+        session_builder = session_builder
+            .add_player(player, i)
+            .expect("failed to add player");
+    }
+
+    // move the channel out of the socket (required because GGRS takes ownership of it)
+    let channel = socket.take_channel(0).unwrap();
+
+    // start the GGRS session
+    let ggrs_session = session_builder
+        .start_p2p_session(channel)
+        .expect("failed to start session");
+
+    commands.insert_resource(bevy_ggrs::Session::P2P(ggrs_session));
 }
